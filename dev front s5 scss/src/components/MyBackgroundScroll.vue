1abc91f860105d348fdea2464bf4a660f555b720
LudoLaFayette <ludovicfaye89@gmail.com>
2023-10-02 16:10:25 +0200

    addingsvg and scrollbkg update

--- dev front s5 scss/src/components/MyBackgroundScroll.vue
+++ dev front s5 scss/src/components/MyBackgroundScroll.vue
@@ -7,16 +7,11 @@ function handleScroll() {
 
     let scrollTop = window.scrollY
     let scrollBottom = scrollTop + window.innerHeight
-
     let offsetTop = container.value.offsetTop
     let elementHeight = container.value.offsetHeight
 
 
     let result = ((scrollBottom - offsetTop) / elementHeight * 100);
-    // this.percent = Math.floor(percent)
-    // console.log(`scrollTop: ${scrollTop}`, `scrollBottom: ${scrollBottom}`)
-    // console.log(offsetTop)
-    // console.log(percent)
     percent.value = result > 100 ? 100 : Math.round(result);
 
 };
@@ -29,34 +24,12 @@ onUnmounted(() => {
     window.removeEventListener("scroll", handleScroll)
 })
 </script>
-<!-- <script>
-export default{
-    data(){
-        return{
-            percent:0
-        }
-    },
-    methods: {
-        
-    },
-    mounted() {
-        
-    },
-    unmounted() {
-       
-    }
-}
-</script> -->
-
 
 <template>
     <div ref="container" class="backgroundScroll">
         <p class="backgroundScroll__content">Section Scroll : {{ percent }}</p>
     </div>
 </template>
-
-
-
 
 <style lang="scss" scoped>
 .backgroundScroll {
